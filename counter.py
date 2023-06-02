fd4d5aff3c06ee69cf5c9a41772add8981169a21
Matt Wyatt <matt.wyatt@veteransunited.com>
2023-06-02 12:52:57 -0500

    1

--- counter.py
+++ counter.py
@@ -1,9 +1,17 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    handle = open(file_name)
 
-
-
+    days = dict()
+    for line in handle:
+        if line.startswith("From"):
+            words = line.split()
+            if len(words) > 2:
+                day = words[2]
+                days[day] = days.get(day,0) +1
+                
+    print(days)
 ## if you want to test locally run > python payCalculator.py
 if __name__ == "__main__":
     countDayOfTheWeek()
